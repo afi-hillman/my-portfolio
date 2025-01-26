f183bb6eda874c02bf2e0d79e9ea3d17c0116261
afi-hillman <afi.hillman17@gmail.com>
2025-01-26 14:40:11 +0800

    initial commit, project setup

---
+++ src/app/sandbox/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Sandbox = () => {
+  return (
+    <div className="flex gap-2">
+      <div className="px-4 bg-background">
+        <p className="text-foreground">BASE</p>
+      </div>
+      <div className="px-4 bg-primary">
+        <p className="text-primary-foreground">PRIMARY</p>
+      </div>
+      <div className="px-4 bg-secondary">
+        <p className="text-secondary-foreground">SECONDARY</p>
+      </div>
+      <div className="px-4 bg-muted">
+        <p className="text-muted-foreground">MUTED</p>
+      </div>
+      <div className="px-4 bg-card">
+        <p className="text-card-foreground">CARD</p>
+      </div>
+      <div className="px-4 bg-destructive">
+        <p className="text-destructive-foreground">DESTRUCTIVE</p>
+      </div>
+    </div>
+  );
+};
+
+export default Sandbox;
