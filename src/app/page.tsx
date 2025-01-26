4062e1b9a08f973bc22a46336224fec987b4b9fd
afi-hillman <afi.hillman17@gmail.com>
2025-01-27 01:06:15 +0800

    feat:added tech stack component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { techStack } from "@/components/techStack";
+
 const Home = () => {
   return (
     <div className="flex flex-col items-center">
@@ -10,9 +12,9 @@ const Home = () => {
       </div>
       <div>
         <h1>SOFTWARE</h1>
-        <div className="flex gap-2">
-          <h1>ENGINEER</h1>
-          <div className="rounded-full bg-primary py-4 px-3 h-[60px] flex gap-2 items-center">
+        <div className="flex items-center gap-2">
+          <h1 className="text-center">ENGINEER</h1>
+          <div className="rounded-full border-ring border py-4 px-3 h-[60px] flex gap-2 items-center hover:bg-primary hover:text-primary-foreground duration-300 cursor-pointer">
             <div className="relative rounded-full bg-green-500 w-4 h-4">
               <div className="rounded-full absolute bg-green-500 animate-ping w-4 h-4"></div>
             </div>
@@ -20,7 +22,28 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div>tech stack</div>
+      <div className="space-y-10 w-full">
+        <div className="space-y-4">
+          <h1>My Tech Stack</h1>
+          <p>list of shiz</p>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-between">
+          {techStack.map((tech, index) => (
+            <div
+              key={index}
+              className="flex flex-grow gap-2 rounded-xl p-2.5 border border-ring hover:bg-foreground/10 duration-300 dark:bg-red-500"
+            >
+              <div className={`p-3 rounded-lg w-fit ${tech.bgColor}`}>
+                {tech.icon}
+              </div>
+              <div className="flex flex-col justify-between">
+                <h4>{tech.name}</h4>
+                <p>{tech.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -27,6 +27,16 @@ const Sandbox = () => {
       <div className="px-4 bg-accent">
         <p className="text-accent-foreground">ACCENT</p>
       </div>
+
+      <div className="relative group">
+        <div className="rounded-full border-ring border py-4 px-3 h-[60px] flex gap-2 items-center  duration-300 cursor-pointer overflow-hidden">
+          <div className="relative rounded-full bg-green-500 w-4 h-4">
+            <div className="rounded-full absolute bg-green-500 animate-ping w-4 h-4"></div>
+          </div>
+          <p>Let&apos;s Connect!</p>
+        </div>
+        <div className="absolute opacity-0 group-hover:opacity-100 top-[60px] left-1/2 transform -translate-x-1/2 w-[150px] h-[60px] bg-blue-500 rounded-full transition-transform duration-300 ease-in-out group-hover:translate-y-[-100%] -z-10"></div>
+      </div>
     </div>
   );
 };
