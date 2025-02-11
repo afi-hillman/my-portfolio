9f6442abee7af0268b6a6c770d88bf355752ce53
afi-hillman <afi.hillman17@gmail.com>
2025-02-11 16:38:02 +0800

    wip:mailto function

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const ContactButton = () => {
   return (
-    <div className="rounded-full border-ring border py-4 px-3 h-[60px] flex gap-2 items-center hover:bg-ring hover:text-primary-foreground duration-300 cursor-pointer transform transition-transform hover:-rotate-12 origin-bottom-left">
-      <div className="relative rounded-full bg-green-500 w-4 h-4">
-        <div className="rounded-full absolute bg-green-500 animate-ping w-4 h-4"></div>
+    <a href="mailto:afi.hillman17@gmail.com">
+      <div className="rounded-full border-ring border py-4 px-3 h-[60px] flex gap-2 items-center hover:bg-ring hover:text-primary-foreground duration-300 cursor-pointer transform transition-transform hover:-rotate-12 origin-bottom-left">
+        <div className="relative rounded-full bg-green-500 w-4 h-4">
+          <div className="rounded-full absolute bg-green-500 animate-ping w-4 h-4"></div>
+        </div>
+        <p>Let&apos;s Connect!</p>
       </div>
-      <p>Let&apos;s Connect!</p>
-    </div>
+    </a>
   );
 };
 
